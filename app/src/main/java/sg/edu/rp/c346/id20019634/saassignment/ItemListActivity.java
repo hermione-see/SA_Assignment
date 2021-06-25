b8c054db631a09d7bb824e65468e4ab50d75f492
hermione-see <20019634@myrp.edu.sg>
2021-06-26 00:30:02 +0800

    update

--- app/src/main/java/sg/edu/rp/c346/id20019634/saassignment/ItemListActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20019634/saassignment/ItemListActivity.java
@@ -3,12 +3,28 @@ package sg.edu.rp.c346.id20019634.saassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class ItemListActivity extends AppCompatActivity {
+
+    EditText etName, etYear, etMonth, etDay;
+    Button btnAdd, btnUpdate, btnDelete;
+    ListView lvProducts;
+    ArrayList<String> alProducts;
+    Spinner spnMonths;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
+
+
+
     }
 }
--- app/src/main/java/sg/edu/rp/c346/id20019634/saassignment/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20019634/saassignment/MainActivity.java
@@ -2,13 +2,47 @@ package sg.edu.rp.c346.id20019634.saassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLearn, btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLearn = findViewById(R.id.buttonWebsite);
+        btnNext = findViewById(R.id.buttonNext);
+
+        btnLearn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intentUrl = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.samsung.com/sg/"));
+                startActivity(intentUrl);
+                Toast.makeText(MainActivity.this, "Proceeding to Website...", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intentItem = new Intent(MainActivity.this, ItemListActivity.class);
+
+                startActivity(intentItem);
+
+            }
+        });
+
     }
 }
